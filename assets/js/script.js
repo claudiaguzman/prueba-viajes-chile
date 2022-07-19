4ee2e66d51fdff4c904eaf7266f1d99753409580
Claudia <claudia.guzmans@usach.cl>
2022-07-18 23:58:41 -0400

    Alerta de enviar mensaje

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,4 +3,8 @@ $(function(){
     tooltipTriggerList.map(function (tooltipTriggerEl) {
         return new bootstrap.Tooltip(tooltipTriggerEl);
     });
+
+    $("#enviarMensaje").on('click', function(){
+        alert("El mensaje fue enviado correctamente...");
+    });
 });
